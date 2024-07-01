3ba163ef9faa5226b76eec24d0a2ff92edc616fa
Migea <qiaozhi22@Gmail.com>
2024-07-01 21:53:17 +0800

    语言更改

---
+++ Assets/Script/StartMenu/LanguageDropdown.cs
@@ -0,0 +1,45 @@
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Localization.Settings;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.Localization;
+
+public class LanguageDropdown : MonoBehaviour
+{
+    public Dropdown languageDropdown;
+
+    void Start()
+    {
+        PopulateDropdown();
+        languageDropdown.onValueChanged.AddListener(delegate { OnLanguageChanged(languageDropdown); });
+    }
+
+    void PopulateDropdown()
+    {
+        languageDropdown.ClearOptions();
+        var options = new List<string>();
+
+        foreach (var locale in LocalizationSettings.AvailableLocales.Locales)
+        {
+            options.Add(locale.name);
+        }
+
+        languageDropdown.AddOptions(options);
+        // Set the current selected locale
+        var selectedLocale = LocalizationSettings.SelectedLocale;
+        languageDropdown.value = LocalizationSettings.AvailableLocales.Locales.IndexOf(selectedLocale);
+    }
+
+    void OnLanguageChanged(Dropdown change)
+    {
+        var selectedLocale = LocalizationSettings.AvailableLocales.Locales[change.value];
+        StartCoroutine(SetLocale(selectedLocale));
+    }
+
+    IEnumerator SetLocale(Locale locale)
+    {
+        yield return LocalizationSettings.InitializationOperation;
+        LocalizationSettings.SelectedLocale = locale;
+    }
+}

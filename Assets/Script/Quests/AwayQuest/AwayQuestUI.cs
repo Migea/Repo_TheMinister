eecb82bf864402f3b8504edf2ac8622e1b74e2f4
Migea <qiaozhi22@Gmail.com>
2024-07-24 11:11:27 +0800

    Update AwayQuestUI.cs

--- Assets/Script/Quests/AwayQuest/AwayQuestUI.cs
+++ Assets/Script/Quests/AwayQuest/AwayQuestUI.cs
@@ -58,7 +58,7 @@ public class AwayQuestUI : MonoBehaviour, ICharacterSelect
         {
             var current = Instantiate(tagTemp, TagContent).GetComponentInChildren<TagWithDescribetion>();
             current.name = tag.ToString();
-            current.Setup(tag);
+            current.SetupSmall(tag);
             current.transform.parent.gameObject.SetActive(true);
         }
         healthText.text = health.ToString();
